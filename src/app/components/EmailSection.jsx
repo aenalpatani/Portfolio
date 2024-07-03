7eeb14b1c9519b47fd2ff22b45d683eb4fd86f8e
aenalpatani <aenalpatani1623@gmail.com>
2024-07-03 18:42:46 +0530

    new update

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -37,7 +37,7 @@ const EmailSection = () => {
     const resData = await response.json();
 
     if (response.status === 200) {
-      console.log("Message sent.");
+      console.log("Message sent");
       setEmailSubmitted(true);
     }
   };
@@ -127,7 +127,7 @@ const EmailSection = () => {
             </div>
             <button
               type="submit"
-              className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+              className="bg-primary-500 hover:bg-gray-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
             >
               Send Message
             </button>
