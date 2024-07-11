c37544d5506e3dc5a5f044c1e8bae6315da41ec2
aenalpatani <aenalpatani1623@gmail.com>
2024-07-11 22:11:36 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
@@ -9,12 +9,12 @@ import HeroSection from "./components/HeroSection"
 
 export default function Home() {
   return (
-    <main className="flex min-h-fit flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-6 sm:mx-auto">
+      <div className="container mt-24 mx-auto px-12 py-6">
         <HeroSection />
         <AboutSection />
-        <ProjectsSection /> 
+        <ProjectsSection />
         <EmailSection />
       </div>
       <Footer />
