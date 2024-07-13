033ce19d4a79f8f48945755332a0513c0ea317b1
aenalpatani <aenalpatani1623@gmail.com>
2024-07-13 09:06:11 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-6">
+      <div className="container mt-24 mx-auto px-12 py-6 text-white">
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
